f3c8413d109e65530b16cc5ad7d1a0b4d628e809
Titus Waldner <titus.waldner@outlook.com>
2025-03-23 14:12:58 -0500

    Adjusted Cmake

---
+++ build/_deps/glm-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/msys64/home/Comrade/robot_arm_ik_simulator/build/_deps/glm-src
+# Build directory: C:/msys64/home/Comrade/robot_arm_ik_simulator/build/_deps/glm-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
